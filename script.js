6a716fa63cdf7e1aa5c9958190d28660a55f256e
rayyane-555 <rexorayyane@gmail.com>
2025-01-12 18:47:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function showScreen(screenNumber) {
+
+  const screens = document.querySelectorAll('.screen');
+  screens.forEach(screen => screen.classList.add('hidden'));
+
+
+  const targetScreen = document.getElementById(`screen${screenNumber}`);
+  if (targetScreen) {
+      targetScreen.classList.remove('hidden');
+  }
+}
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('sliderValue');
+
+slider.addEventListener('input', () => {
+  sliderValue.textContent = slider.value;
+});
+
+document.getElementById('nameForm').addEventListener('submit', function(event) {
+  event.preventDefault();  
+  document.getElementById('nameInput').value = ''; 
+});
+document.getElementById('emailform').addEventListener('submit', function(event) {
+  event.preventDefault();  
+  document.getElementById('emailInput').value = '';  
+});
+document.getElementById('datelform').addEventListener('submit', function(event) {
+  event.preventDefault();  
+  document.getElementById('date').value = '';  
+});
+
